1dd59ff1d832264d51ad25dcc6adea8360d1a2bb
Alexander Kramer <alexander.kramer@trustbit.tech>
2022-11-14 15:08:34 +0100

    enabled scheduling

--- src/main/kotlin/foo/bar/springbootquartz/SpringBootQuartzApplication.kt
+++ src/main/kotlin/foo/bar/springbootquartz/SpringBootQuartzApplication.kt
@@ -2,8 +2,10 @@ package foo.bar.springbootquartz
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class SpringBootQuartzApplication
 
 fun main(args: Array<String>) {
