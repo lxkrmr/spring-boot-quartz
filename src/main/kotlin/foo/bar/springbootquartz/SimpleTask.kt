089d9e21975cde0fca94c1fff9bb064088faed86
Alexander Kramer <alexander.kramer@trustbit.tech>
2022-11-14 16:19:28 +0100

    include port in SimpleTask

--- src/main/kotlin/foo/bar/springbootquartz/SimpleTask.kt
+++ src/main/kotlin/foo/bar/springbootquartz/SimpleTask.kt
@@ -1,12 +1,13 @@
 package foo.bar.springbootquartz
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.time.Instant
 
 @Component
-class SimpleTask {
+class SimpleTask(@Value("\${server.port}") private val port: String) {
 
     @Scheduled(fixedRate = 5_000)
-    fun printAString() = println("######## This is a string at ${Instant.now()}")
+    fun portAndTime() = println("######## Running on port: $port at ${Instant.now()}")
 }
