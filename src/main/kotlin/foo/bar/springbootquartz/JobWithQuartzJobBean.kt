337fea21d542522d0ed78bdda8693f23ef0d5d9b
Alexander Kramer <alexander.kramer@trustbit.tech>
2022-11-15 09:37:34 +0100

    add missing component annotation

--- src/main/kotlin/foo/bar/springbootquartz/JobWithQuartzJobBean.kt
+++ src/main/kotlin/foo/bar/springbootquartz/JobWithQuartzJobBean.kt
@@ -3,8 +3,10 @@ package foo.bar.springbootquartz
 import org.quartz.JobExecutionContext
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.quartz.QuartzJobBean
+import org.springframework.stereotype.Component
 import java.time.Instant
 
+@Component
 class JobWithQuartzJobBean(@Value("\${server.port}") private val port: String) : QuartzJobBean() {
 
     override fun executeInternal(context: JobExecutionContext) =
